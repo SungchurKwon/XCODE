6dfcf7179ca39d81b8e0b8cf94ca584b615bd813
sungchurKwon <sungcherk@gmail.com>
2021-02-21 15:52:59 +0900

    part 3 mid proc

--- test_instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ test_instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -9,18 +9,46 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    struct Constans{
+        static let cornerRadius:CGFloat =  8.0
+    }
     private let usernameEmailField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Username or Email..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constans.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
         return UITextField()
     }()
     
     private let passwordField: UITextField = {
         let field = UITextField()
+        field.placeholder = "password..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constans.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
         field.isSecureTextEntry = true
         return field
     }()
     
     private let loginButton: UIButton = {
-        return UIButton()
+        let button =  UIButton()
+        button.setTitle("Log in", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constans.cornerRadius
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        return button
     }()
     
     private let termsButton: UIButton = {
@@ -32,11 +60,14 @@ class LoginViewController: UIViewController {
     }()
     
     private let createAccountButton: UIButton = {
-        return UIButton()
+        let button =  UIButton()
+        button.setTitleColor(.label, for: .normal )
+        button.setTitle("New user? Create an Account", for: .normal)
+        return button
     }()
     
     private let headerView: UIView = {
-        return UIView()
+        let header =  UIView()
     }()
     
     override func viewDidLoad() {
@@ -47,6 +78,10 @@ class LoginViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         //assigne frames
+        headerView.frame = CGRect(x: 0,
+                                  y: view.safeAreaInsets.top,
+                                  width: view.width,
+                                  height: 200)
     }
     
     private func addSubviews(){
---
+++ test_instagram/Resources/Extension.swift
@@ -0,0 +1,36 @@
+//
+//  Extension.swift
+//  test_instagram
+//
+//  Created by 권성철 on 2021/02/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var botom: CGFloat {
+        return frame.origin.y + frame.size.width
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
